f65d0e0e8c05abae751bf7de8b6040a0e82556c2
Erick Clinton <erickccleite@gmail.com>
2025-06-07 12:23:13 -0300

    feat: create dtos

---
+++ Desafio-Itau/Application/Asset/Asset.Contract/DTOs/CreateAssetDto.cs
@@ -0,0 +1,7 @@
+namespace DesafioInvestimentosItau.Application.User.User.Client.DTOs;
+
+public class CreateAssetDto
+{
+    public string Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
---
+++ Desafio-Itau/Application/Quote/Quote.Contract/Quote.Contract/DTOs/CreateQuoteDto.cs
@@ -0,0 +1,8 @@
+namespace DesafioInvestimentosItau.Application.Quote.Quote.Contract.Quote.Contract.DTOs;
+
+public class CreateQuoteDto
+{
+    public long AssetId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public DateTime Timestamp { get; set; }
+}
