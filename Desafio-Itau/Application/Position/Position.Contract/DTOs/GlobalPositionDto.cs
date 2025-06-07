bf8d163d730102477adbbcada9eb84fb61d55c6d
Erick Clinton <erickccleite@gmail.com>
2025-06-07 10:35:07 -0300

    feat(Application): creating interfaces that the service will implement

---
+++ Desafio-Itau/Application/Position/Position.Contract/DTOs/GlobalPositionDto.cs
@@ -0,0 +1,7 @@
+namespace DesafioInvestimentosItau.Application.Position.Position.Contract.DTOs;
+
+public class GlobalPositionDto
+{
+    public long UserId { get; set; }
+    public decimal TotalProfitLoss { get; set; }
+}
