bb6acf3945c89214f2c2ece61ab4b5001480980a
Erick Clinton <erickccleite@gmail.com>
2025-06-07 10:53:53 -0300

    feat(Infraestructure): create query for get buy trades by user and asset

--- Desafio-Itau/Infrastructure/Repositories/TradeRepository.cs
+++ Desafio-Itau/Infrastructure/Repositories/TradeRepository.cs
@@ -15,6 +15,16 @@ public class TradeRepository : ITradeRepository
         _context = context;
     }
     
+    public async Task<List<TradeEntity>> GetBuyTradesByUserAndAssetAsync(long userId, string assetCode)
+    {
+        return await _context.Trades
+            .Include(t => t.Asset)
+            .Where(t => t.UserId == userId &&
+                        t.Type == TradeTypeEnum.Buy &&
+                        t.Asset.Code == assetCode)
+            .ToListAsync();
+    }
+    
     public async Task<List<TradeEntity>> GetGroupedBuyTradesByUserAsync(long userId)
     {
         return await _context.Trades
