0ad56ffe95f4ed971e62dddca6054b8f0aa7bd73
Erick Clinton <erickccleite@gmail.com>
2025-06-07 10:37:37 -0300

    feat(Application): create contract for method GetGroupedBuyTradesByUserAsync for repository

--- Desafio-Itau/Application/Trade/Trade.Contract/Interfaces/ITradeRepository.cs
+++ Desafio-Itau/Application/Trade/Trade.Contract/Interfaces/ITradeRepository.cs
@@ -1,7 +1,11 @@
+using DesafioInvestimentosItau.Application.Trade.Trade.Contract.DTOs;
+using DesafioInvestimentosItau.Domain.Entities;
+
 namespace DesafioInvestimentosItau.Application.User.User.Client;
 
 public interface ITradeRepository
 {
     Task<decimal> GetTotalInvestedAsync(long userId);
     Task<decimal> GetTotalBrokerageFeeAsync(long userId);
+    Task<List<TradeEntity>>GetGroupedBuyTradesByUserAsync(long userId);
 }
