f0f247990eb88ac2e6404ea1f8b08724f0801091
Erick Clinton <erickccleite@gmail.com>
2025-06-07 23:47:07 -0300

    fix(Domain): remove name

--- Desafio-Itau/Domain/Entities/AssetEntity.cs
+++ Desafio-Itau/Domain/Entities/AssetEntity.cs
@@ -9,7 +9,6 @@ public class AssetEntity
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long Id { get; set; }
     public string Code { get; set; } = string.Empty; 
-    public string Name { get; set; } = string.Empty;
 
     public List<TradeEntity> Trades { get; set; } = new();
     public List<QuoteEntity> Quotes { get; set; } = new();
