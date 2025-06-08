eed92cc718d042c9101df11c14af005ec5240f87
Erick Clinton <erickccleite@gmail.com>
2025-06-07 23:50:05 -0300

    feat(Infraestructure): create methods

--- Desafio-Itau/Infrastructure/Repositories/UserRepository.cs
+++ Desafio-Itau/Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 using DesafioInvestimentosItau.Application.User.User.Client;
+using DesafioInvestimentosItau.Application.User.User.Contracts.Interfaces;
 using DesafioInvestimentosItau.Domain.Entities;
 using DesafioInvestimentosItau.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -21,8 +22,18 @@ public class UserRepository : IUserRepository
         return user;
     }
 
+    public async Task<UserEntity?> GetByIdAsync(long id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
     public async Task<IEnumerable<UserEntity>> GetAllAsync()
     {
         return await _context.Users.ToListAsync();
+    }
+
+    public async Task<bool> ExistsAsync(string email)
+    {
+        return await _context.Users.AnyAsync(u => u.Email == email);
     }
 }
